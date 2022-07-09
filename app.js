4a92e460d0182748248e2a4569917183f3d9fa3c
Anex P B <97126806+anexbensi@users.noreply.github.com>
2022-07-09 15:29:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,8 +30,9 @@ app.use(session({
   store: new MemoryStore({
     checkPeriod: 86400000 // prune expired entries every 24h
   }),
-  resave: false,
-  secret: 'keyboard cat'
+  secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
 }))
 db.connect((err)=>{
   if(err){
